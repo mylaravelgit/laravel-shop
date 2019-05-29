d9f5d6fb2e4d9bfd192f1736cdee589232682c35
mylaravelgit <1021513892@qq.com>
2019-05-29 14:57:34 +0800

    收货地址列表

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,8 @@ Route::get('/', 'PagesController@root')->name('root');
 
 
 Auth::routes(['verify' => true]);
+//, 'verified'
+Route::group(['middleware' => ['auth']], function() {
+    Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+});
 
